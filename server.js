f59ff7d558118767f608624d6ded222eb30dbc1f
pranit121 <pranitlahane@gmail.com>
2018-11-15 19:56:14 +0530

    Update : Register DB insertion done.

--- server.js
+++ server.js
@@ -2,8 +2,21 @@ var express = require('express');
 var app = express();
 var path = require('path');
 app.use(express.static('public'))
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "society"
+});
+
+
 
 app.get('/home', function(req, res){
+
    res.sendFile(__dirname + '/views/home.html');
 });
 
@@ -34,14 +47,31 @@ app.post('/review_registration', function(req, res){
 });
 
 app.post('/register_committee', function(req, res){
-   res.sendFile(__dirname + '/views/register_committee.html');
+console.log("Running at Port 3000");
+
+
+ var name=req.body.name;
+  var number=req.body.number;
+  var address=req.body.address;
+  var no=req.body.no;
+
+console.log(req.body);
+  console.log('You sent the name "' + name+'".\n');
+
+
+  var sql = "INSERT INTO info (name, Email,City ,Pincode) VALUES ('"+name+"', '"+number+"','"+address+"','"+no+"')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+    });
+res.sendFile(__dirname + '/views/register_committee.html');
 });
+
 
 
 app.post('/society_home', function(req, res){
    res.sendFile(__dirname + '/views/society_home.html');
 });
 
-
-
 app.listen(3000);
+
