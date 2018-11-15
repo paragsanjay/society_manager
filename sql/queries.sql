179ecaddcad4400675dae96b66e04d73078332cd
pranit121 <pranitlahane@gmail.com>
2018-11-15 20:06:50 +0530

    sql query added

---
+++ sql/queries.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `info` (
+  `name` varchar(100) DEFAULT NULL,
+  `Email` varchar(40) DEFAULT NULL,
+  `City` varchar(40) DEFAULT NULL,
+  `Pincode` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
