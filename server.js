2d8f24051e8ed90de1c563c92b7c1f767a32cf47
pranit121 <pranitlahane@gmail.com>
2018-12-08 14:30:21 +0530

    S3 database connected

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 var express = require('express');
-var app = express();
+var app = require('express')();
 var path = require('path');
-app.use(express.static('public'))
+var router = express.Router();
+var multer  =   require('multer');
+var upload = multer({dest:'/public'})
+
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+app.use(express.static(__dirname + "/public"));
 app.set("view engine","jade")
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "password",
-  database: "society"
+  database: "s3"
 });
 
 
@@ -32,14 +40,8 @@ con.query("SELECT * FROM info", function (err, result, fields) {
 
 app.get('/login',function(req,res){
 
-con.query('select * from info', function (err, recordset) {
-            console.log(recordset);
-            if (err)
-                console.log(err)
-            else
-                res.render('login', { login: recordset });
 
-        });
+   res.sendFile(__dirname + '/views/login.html');
 });
 
 
@@ -49,12 +51,45 @@ con.query('select * from info', function (err, recordset) {
 
 
 
-app.get('/register', function(req, res){
-   res.sendFile(__dirname + '/views/register.html');
+app.post('/register', function(req, res){
+
+var username= req.body.username;
+var password = req.body.password;
+console.log(username);
+console.log(password);
+
+con.query('SELECT * FROM user WHERE username = ?',[username], function (error, results, fields) {
+console.log("Running at Port 3000");
+if (error) {
+  console.log("error ocurred",error);
+
+  }else{
+  console.log('The solution is: ', results);
+
+      if(results.length >0){
+      if(results[0].password == password){
+       res.sendFile(__dirname + '/views/register.html');
+      }
+      else{
+    console.log("erroror");
+     res.sendFile(__dirname + '/login.html')
+    }
+  }
+
+}
 });
 
-app.post('/register_members', function(req, res){
-   res.sendFile(__dirname + '/views/register_members.html');
+
+});
+
+app.post('/report', function(req, res){
+var form = new formidable.IncomingForm();
+   form.parse(req, function (err, fields, files) {
+      var oldpath = files.finger.path;
+      var newpath = 'Register_thumb/' + files.finger.name;
+      fs.createReadStream(oldpath).pipe(fs.createWriteStream(newpath));
+      });
+   res.sendFile(__dirname + '/views/report.html');
 });
 
 
@@ -71,25 +106,33 @@ app.post('/review_registration', function(req, res){
    res.sendFile(__dirname + '/views/review_registration.html');
 });
 
-app.post('/register_committee', function(req, res){
-console.log("Running at Port 3000");
+app.post('/finger', function(req, res){
 
-
- var name=req.body.name;
-  var reg_no=req.body.reg_no;
-  var address=req.body.address;
-  var phno=req.body.phno;
+var name=req.body.name;
+  var email=req.body.email;
+  var cls=req.body.cls;
+  var rno=req.body.rno;
+  var finger=req.body.finger;
 
 console.log(req.body);
+
   console.log('You sent the name "' + name+'".\n');
 
 
-  var sql = "INSERT INTO info (name,reg_no,address,phno) VALUES ('"+name+"', '"+reg_no+"','"+address+"','"+phno+"')";
+  var sql = "INSERT INTO student (name , email,class ,rollno, finger) VALUES ('"+name+"', '"+email+"','"+cls+"','"+rno+"','"+finger+"')";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("1 record inserted");
     });
-res.sendFile(__dirname + '/views/register_committee.html');
+
+    var form = new formidable.IncomingForm();
+   form.parse(req, function (err, fields, files) {
+      var oldpath = files.finger.path;
+      var newpath = 'thumb/' + files.finger.name;
+      fs.createReadStream(oldpath).pipe(fs.createWriteStream(newpath));
+      });
+
+res.sendFile(__dirname + '/views/finger.html');
 });
 
 
